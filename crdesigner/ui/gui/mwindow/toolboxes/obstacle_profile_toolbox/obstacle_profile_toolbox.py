4eaed351b1ff4409299f3cd3bdfbf9fe4bfdaede
Tim <ge78luy@mytum.de>
2023-02-03 14:24:00 +0100

    multiple selection done

--- crdesigner/ui/gui/mwindow/toolboxes/obstacle_profile_toolbox/obstacle_profile_toolbox.py
+++ crdesigner/ui/gui/mwindow/toolboxes/obstacle_profile_toolbox/obstacle_profile_toolbox.py
@@ -50,6 +50,11 @@ class ObstacleProfileToolbox(QDockWidget):
         self.pos = []
         self.temp_obstacle = None
 
+        self.xmin = 0
+        self.xmax = 0
+        self.ymin = 0
+        self.ymax = 0
+
         if SUMO_AVAILABLE:
             self.sumo_simulation = SUMOSimulation(tmp_folder=tmp_folder)
         else:
@@ -70,14 +75,19 @@ class ObstacleProfileToolbox(QDockWidget):
         adds functionality to gui elements like buttons, menus etc
         """
         self.obstacle_profile_toolbox_ui.obstacle_state_variable.currentTextChanged.connect(
-                lambda: self.plot_obstacle_state_profile())
+                lambda: self.plot_obstacles_state_profile())
 
-        self.obstacle_profile_toolbox_ui.selected_obstacle.currentTextChanged.connect(
+        self.current_time.valueChanged.connect(
+                lambda: self.update_animation())
+
+        self.obstacle_profile_toolbox_ui.obstacle_button.clicked.connect(
                 lambda: self.update_obstacle_information())
 
-        self.current_time.valueChanged.connect(lambda: self.update_animation())
+        self.obstacle_selection_ui.button_ok.clicked.connect(
+                lambda: self.update_obstacle_information())
 
-        self.obstacle_profile_toolbox_ui.obstacle_button.clicked.connect(lambda: self.show_obstacle_selection())
+        self.obstacle_profile_toolbox_ui.obstacle_button.clicked.connect(
+                lambda: self.show_obstacle_selection())
 
     def refresh_toolbox(self, scenario: Scenario):
         self.current_scenario = scenario
@@ -139,8 +149,7 @@ class ObstacleProfileToolbox(QDockWidget):
         time = [0]
         obstacle_profiles = []
         state_variable_name = self.obstacle_profile_toolbox_ui.obstacle_state_variable.currentText()
-
-        if len(self.obstacle_selection_ui.formLayout.count()) > 0:
+        if self.obstacle_selection_ui.formLayout.count() > 0:
             for obstacle in self.get_current_obstacles():
                 profile = None  # some simulations do not have their obstacle_states initialized like others and if
                 # trying to access them  # CR will crash
@@ -224,109 +233,10 @@ class ObstacleProfileToolbox(QDockWidget):
                         time = [obstacle.initial_state.time_step]
                         time += [state.time_step for state in obstacle.prediction.trajectory.state_list]
 
-            self.xmin = None
-            self.xmax = None
-            self.ymin = None
-            self.ymax = None
-
-    def plot_obstacle_state_profile(self):
-        """
-        Gets the values based on which profile is selected.
-        If non updated changes, these values come from the xyova array,
-        otherwise directly from the obstacle state_list
-        """
-        print(self.get_current_obstacles())
-
-        if self.obstacle_profile_toolbox_ui.selected_obstacle.currentText() not in ["",
-                                                                                    "None"] and not self.update_ongoing:
-            profile = None
-            obstacle = self.get_current_obstacle()
-            state_variable_name = self.obstacle_profile_toolbox_ui.obstacle_state_variable.currentText()
-            # some simulations do not have their obstacle_states initialized like others and if trying to access them
-            # CR will crash
-            try:
-                if obstacle.initial_state.__getattribute__(state_variable_name):
-                    if isinstance(obstacle, DynamicObstacle):
-                        if state_variable_name == "velocity":
-                            if self.xyova:
-                                profile = [j[3] for j in self.xyova]
-                            else:
-                                profile = [obstacle.initial_state.__getattribute__("velocity")]
-                                profile += [state.__getattribute__("velocity") for state in
-                                            obstacle.prediction.trajectory.state_list]
-                        elif state_variable_name == "acceleration":
-                            if self.xyova:
-                                profile = [j[4] for j in self.xyova]
-                            else:
-                                profile = [obstacle.initial_state.__getattribute__("acceleration")]
-                                profile += [state.__getattribute__("acceleration") for state in
-                                            obstacle.prediction.trajectory.state_list]
-                        elif state_variable_name == "yaw_rate":
-                            if self.xyova:
-                                profile = [j[5] for j in self.xyova]
-                            else:
-                                profile = [obstacle.initial_state.__getattribute__("yaw_rate")]
-                                profile += [state.__getattribute__("yaw_rate") for state in
-                                            obstacle.prediction.trajectory.state_list]
-                        elif state_variable_name == "slip_angle":
-                            if self.xyova:
-                                profile = [j[6] for j in self.xyova]
-                            else:
-                                profile = [obstacle.initial_state.__getattribute__("slip_angle")]
-                                profile += [state.__getattribute__("slip_angle") for state in
-                                            obstacle.prediction.trajectory.state_list]
-                # states which Static and DynamicObstacles share
-                if state_variable_name == "x-position":
-                    if isinstance(obstacle, StaticObstacle):
-                        profile = [obstacle.initial_state.__getattribute__("position")[0]]
-                    elif isinstance(obstacle, DynamicObstacle):
-                        if self.xyova:
-                            profile = [j[0] for j in self.xyova]
-                        else:
-                            profile = [obstacle.initial_state.__getattribute__("position")[0]]
-                            profile += [state.__getattribute__("position")[0] for state in
-                                        obstacle.prediction.trajectory.state_list]
-                elif state_variable_name == "y-position":
-                    if isinstance(obstacle, StaticObstacle):
-                        profile = [obstacle.initial_state.__getattribute__("position")[1]]
-                    elif isinstance(obstacle, DynamicObstacle):
-                        if self.xyova:
-                            profile = [j[1] for j in self.xyova]
-                        else:
-                            profile = [obstacle.initial_state.__getattribute__("position")[1]]
-                            profile += [state.__getattribute__("position")[1] for state in
-                                        obstacle.prediction.trajectory.state_list]
-                elif state_variable_name == "orientation":
-                    if isinstance(obstacle, StaticObstacle):
-                        profile = [obstacle.initial_state.__getattribute__("orientation")]
-                    elif isinstance(obstacle, DynamicObstacle):
-                        if self.xyova:
-                            profile = [j[2] for j in self.xyova]
-                        else:
-                            profile = [obstacle.initial_state.__getattribute__("orientation")]
-                            profile += [state.__getattribute__("orientation") for state in
-                                        obstacle.prediction.trajectory.state_list]
-            except:
-                print(state_variable_name + " not initialized on")
-
-            if isinstance(obstacle, DynamicObstacle) and not self.obstacle_profile_toolbox_ui.animation.isChecked():
-                if self.xyova:
-                    time = [i for i in range(0, len(self.xyova))]
-                else:
-                    time = [obstacle.initial_state.time_step]
-                    time += [state.time_step for state in obstacle.prediction.trajectory.state_list]
-            else:
-                time = [0]
-
-            self.xmin = None
-            self.xmax = None
-            self.ymin = None
-            self.ymax = None
-
-            if profile and self.obstacle_profile_toolbox_ui.animation.isChecked():
-                self.animate_plot(profile)
-            elif profile:
-                self.draw_plot(time, profile)
+            if obstacle_profiles and self.obstacle_profile_toolbox_ui.animation.isChecked():
+                self.animate_plot(obstacle_profiles)
+            elif obstacle_profiles:
+                self.draw_plot(time, obstacle_profiles)
 
     @staticmethod
     def resolve_y_label(state_variable_name: str) -> str:
@@ -359,32 +269,38 @@ class ObstacleProfileToolbox(QDockWidget):
         retrieves obstacle details to the gui when an obstacle is pressed or the id
         is selected in the obstacle toolbox
         """
-        if self.obstacle_profile_toolbox_ui.selected_obstacle.currentText() not in ["", "None"]:
+        self.update_ongoing = True
+        obstacle = self.get_current_obstacles()
 
-            self.update_ongoing = True
-            obstacle = self.get_current_obstacle()
-
-            self.obstacle_profile_toolbox_ui.obstacle_type.setCurrentText(obstacle.obstacle_type.value)
+        if obstacle:
+            print(obstacle)
+            print(obstacle[0])
             self.obstacle_profile_toolbox_ui.obstacle_state_variable.clear()
-            state_variables = [var for var in obstacle.initial_state.attributes if var not in ["position", "time_step"]]
+            state_variables = [var for var in obstacle[0].initial_state.attributes if var not in ["position", "time_step"]]
 
-            if "position" in obstacle.initial_state.attributes:
+            if "position" in obstacle[0].initial_state.attributes:
                 state_variables += ["x-position", "y-position"]
 
             self.obstacle_profile_toolbox_ui.obstacle_state_variable.addItems(state_variables)
             self.update_ongoing = False
             # clear xyo if switch to another obstacle
             self.xyova.clear()
-
-            self.plot_obstacle_state_profile()
-
+            self.plot_obstacles_state_profile()
         # if set to "None": clear QLineEdits
         else:
             self.obstacle_profile_toolbox_ui.obstacle_state_variable.clear()
             self.obstacle_profile_toolbox_ui.figure.clear()
             self.obstacle_profile_toolbox_ui.canvas.draw()
 
-    def draw_plot(self, time, profile, xmin: float = None, xmax: float = None, ymin: float = None, ymax: float = None):
+    def get_max_min(self, profiles):
+        print("TODO")
+        for p in profiles:
+            if min(p) < self.ymin:
+                self.ymin = min(p)
+            if max(p) > self.ymax:
+                self.ymax = max(p)
+
+    def draw_plot(self, time, profiles, xmin: float = None, xmax: float = None, ymin: float = None, ymax: float = None):
         """
         draws the state plot in the obstacle toolbox
         :param time: time steps
@@ -394,20 +310,23 @@ class ObstacleProfileToolbox(QDockWidget):
         :param ymin: y lower bound to be drawn
         :param: ymax: y upper bound to be drawn
         """
+        self.get_max_min(profiles)
+
         state_variable_name = self.obstacle_profile_toolbox_ui.obstacle_state_variable.currentText()
         # clear previous profile
         self.obstacle_profile_toolbox_ui.figure.clear()
         # create an axis
         ax = self.obstacle_profile_toolbox_ui.figure.add_subplot(111)
         # plot data
-        ax.plot(time, profile, '.-', markersize=4)
+        for p in profiles:
+            ax.plot(time, p, '.-', markersize=4)
         ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.1f}'))
         ax.set_xlabel("time [s]")
         ax.set_ylabel(self.resolve_y_label(state_variable_name))
         self.obstacle_profile_toolbox_ui.figure.tight_layout()
 
         # to get reasonable limits. If the difference is very small: it will be difficult to make changes
-        ax.set_ylim([min(profile) - 0.5, max(profile) + 0.5])
+        ax.set_ylim([self.ymin - 0.5, self.ymax + 0.5])
         # if zoomed in the new plot should be drawn with previous x and y limits
         # (so it doesn't zoom out on mouse event if zoomed in)
         if self.xmin and self.xmax and self.ymin and self.ymax:
@@ -419,9 +338,9 @@ class ObstacleProfileToolbox(QDockWidget):
         ax.callbacks.connect('ylim_changed', self.on_ylim_change)
 
     def update_animation(self):
-        self.plot_obstacle_state_profile()
+        self.plot_obstacles_state_profile()
 
-    def animate_plot(self, profile):
+    def animate_plot(self, profiles):
         """
         draws the state plot in the obstacle toolbox
         :param time: time steps
@@ -433,11 +352,16 @@ class ObstacleProfileToolbox(QDockWidget):
         """
         time = [i for i in range(0, self.current_time.value() + 1)]
 
-        # there are some scenarios where more time_steps exist than profile values
-        # this if case prevents a crash
-        if len(time) != len(profile[0:self.current_time.value() + 1]):
-            return
-        self.draw_plot(time, profile[0:self.current_time.value() + 1])
+        current_time_profiles = []
+
+        for p in profiles:
+            current_time_profiles.append(p[0:self.current_time.value() + 1])
+            # there are some scenarios where more time_steps exist than profile values
+            # this if case prevents a crash
+            if len(time) != len(p[0:self.current_time.value() + 1]):
+                return
+
+        self.draw_plot(time, current_time_profiles)
 
     def show_obstacle_selection(self):
         self.connect_events()
